3d9d914f924be368a8e133d9e09b3a7a11a07141
GnanaMoorthy550 <47858183+GnanaMoorthy550@users.noreply.github.com>
2023-09-03 18:34:33 +0530

    Mail Project
    The Mail Project is an open-source email management and automation tool designed to simplify email-related tasks and processes. It provides a set of features and utilities to enhance email communication and productivity.

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "" && textBox1.Text != null) && (textBox2.Text != "" && textBox2.Text != null)) {
+                if (textBox3.Text != "" && textBox3.Text != null)
+                {
+                    MailMessage mailMessage = new MailMessage();
+                    mailMessage.To.Add(textBox3.Text);
+                    mailMessage.From = new MailAddress(textBox1.Text);
+                    mailMessage.Subject = textBox4.Text;
+                    mailMessage.Body = textBox5.Text;
+
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                    smtp.EnableSsl = true;
+                    smtp.Port = 587;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(textBox1.Text, textBox2.Text);
+                    smtp.UseDefaultCredentials = false;
+                    try
+                    {
+                        smtp.Send(mailMessage);
+                        MessageBox.Show("Email Sent","Email",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else {
+                    error.Visible = true;
+                    error.Text = "Please Enter the To Email Address !";
+                    return;
+                }
+            } else
+            {
+                error.Visible = true;
+                error.Text = "Please Enter the Mail id And Password !";
+                return;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            error.Visible = false;
+        }
+    }
+}
